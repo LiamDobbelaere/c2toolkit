d1bfece8ebd4870f979da8a1eb5c017eed7821d2
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2021-12-10 11:32:23 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace c2toolkit
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblTWTDropzone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTWTDropzone
+            // 
+            this.lblTWTDropzone.AllowDrop = true;
+            this.lblTWTDropzone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.lblTWTDropzone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTWTDropzone.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTWTDropzone.ForeColor = System.Drawing.Color.White;
+            this.lblTWTDropzone.Location = new System.Drawing.Point(0, 0);
+            this.lblTWTDropzone.Name = "lblTWTDropzone";
+            this.lblTWTDropzone.Size = new System.Drawing.Size(800, 473);
+            this.lblTWTDropzone.TabIndex = 1;
+            this.lblTWTDropzone.Text = resources.GetString("lblTWTDropzone.Text");
+            this.lblTWTDropzone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTWTDropzone.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblTWTDropzone_DragDrop);
+            this.lblTWTDropzone.DragOver += new System.Windows.Forms.DragEventHandler(this.lblTWTDropzone_DragOver);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 473);
+            this.Controls.Add(this.lblTWTDropzone);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(512, 512);
+            this.Name = "frmMain";
+            this.Text = "C2 Toolkit";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTWTDropzone;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using ToxicRagers.Carmageddon2.Formats;
+
+namespace c2toolkit
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTWTDropzone_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void lblTWTDropzone_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+                if (files != null && files.Length > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        string fileExtension = Path.GetExtension(file);
+
+                        if (fileExtension.ToLower() == ".twt")
+                        {
+                            ExtractTWT(file);
+                        }
+                        else if (fileExtension.ToLower() == ".p16" || fileExtension.ToLower() == ".p08")
+                        {
+                            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                            string srcDir = Path.GetDirectoryName(file);
+
+                            ProcessStartInfo startInfo = new ProcessStartInfo
+                            {
+                                FileName = Path.Combine(exeDirectory, "Resources\\TRixx.exe"),
+                                Arguments = "\"" + file + "\"",
+                                WorkingDirectory = srcDir,
+                                RedirectStandardInput = true,
+                                RedirectStandardOutput = true,
+                                UseShellExecute = false
+                            };
+                            Process p = Process.Start(startInfo);
+                            while (!p.StandardOutput.EndOfStream)
+                            {
+                                string line = p.StandardOutput.ReadLine();
+                                if (line != null && line.Contains("any key"))
+                                {
+                                    p.StandardInput.Write('y');
+                                    break;
+                                }
+                            }
+                            p.WaitForExit();
+
+                            try
+                            {
+                                Directory.Move(Path.Combine(srcDir, "pixies"), Path.Combine(srcDir, "tiffrgb"));
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("tiffrgb folder probably already exists!", "C2 Toolkit", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            CreateTWT(file);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "C2 Toolkit", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ExtractTWT(string file)
+        {
+            string destinationDirectory = Path.GetDirectoryName(file);
+            string actualName = Path.GetFileNameWithoutExtension(file);
+            string targetPath = Path.Combine(destinationDirectory, actualName);
+
+            Directory.CreateDirectory(targetPath);
+
+            TWT twtFile = TWT.Load(file);
+
+            foreach (TWTEntry entry in twtFile.Contents)
+            {
+                twtFile.Extract(entry, targetPath);
+            }
+        }
+
+        private void CreateTWT(string file)
+        {
+            string targetPath = Path.ChangeExtension(file, ".TWT");
+
+            TWT newTWT = TWT.Create(targetPath);
+
+            foreach (string fileName in Directory.GetFiles(file))
+            {
+                newTWT.Add(fileName);
+            }
+
+            newTWT.Save();
+        }
+    }
+}
