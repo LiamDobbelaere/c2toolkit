a73890d8735fe84e48d676ede36f27c57d0cb022
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2021-12-11 16:20:03 +0100

    Add car texture renaming

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,13 @@ namespace c2toolkit
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.lblTWTDropzone = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpMulti = new System.Windows.Forms.TabPage();
+            this.tpRename = new System.Windows.Forms.TabPage();
+            this.lblCarRenameDropzone = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tpMulti.SuspendLayout();
+            this.tpRename.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTWTDropzone
@@ -42,23 +49,83 @@ namespace c2toolkit
             this.lblTWTDropzone.ForeColor = System.Drawing.Color.White;
             this.lblTWTDropzone.Location = new System.Drawing.Point(0, 0);
             this.lblTWTDropzone.Name = "lblTWTDropzone";
-            this.lblTWTDropzone.Size = new System.Drawing.Size(800, 473);
+            this.lblTWTDropzone.Size = new System.Drawing.Size(776, 423);
             this.lblTWTDropzone.TabIndex = 1;
             this.lblTWTDropzone.Text = resources.GetString("lblTWTDropzone.Text");
             this.lblTWTDropzone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lblTWTDropzone.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblTWTDropzone_DragDrop);
             this.lblTWTDropzone.DragOver += new System.Windows.Forms.DragEventHandler(this.lblTWTDropzone_DragOver);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpMulti);
+            this.tabControl1.Controls.Add(this.tpRename);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.tabControl1.Location = new System.Drawing.Point(8, 8);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(0, 0);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(784, 457);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tpMulti
+            // 
+            this.tpMulti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.tpMulti.Controls.Add(this.lblTWTDropzone);
+            this.tpMulti.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tpMulti.ForeColor = System.Drawing.Color.White;
+            this.tpMulti.Location = new System.Drawing.Point(4, 30);
+            this.tpMulti.Margin = new System.Windows.Forms.Padding(0);
+            this.tpMulti.Name = "tpMulti";
+            this.tpMulti.Size = new System.Drawing.Size(776, 423);
+            this.tpMulti.TabIndex = 0;
+            this.tpMulti.Text = "Multitool";
+            // 
+            // tpRename
+            // 
+            this.tpRename.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.tpRename.Controls.Add(this.lblCarRenameDropzone);
+            this.tpRename.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.tpRename.ForeColor = System.Drawing.Color.White;
+            this.tpRename.Location = new System.Drawing.Point(4, 30);
+            this.tpRename.Name = "tpRename";
+            this.tpRename.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRename.Size = new System.Drawing.Size(776, 423);
+            this.tpRename.TabIndex = 1;
+            this.tpRename.Text = "Car texture renaming";
+            // 
+            // lblCarRenameDropzone
+            // 
+            this.lblCarRenameDropzone.AllowDrop = true;
+            this.lblCarRenameDropzone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(30)))), ((int)(((byte)(20)))));
+            this.lblCarRenameDropzone.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblCarRenameDropzone.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarRenameDropzone.ForeColor = System.Drawing.Color.White;
+            this.lblCarRenameDropzone.Location = new System.Drawing.Point(3, 3);
+            this.lblCarRenameDropzone.Name = "lblCarRenameDropzone";
+            this.lblCarRenameDropzone.Size = new System.Drawing.Size(770, 417);
+            this.lblCarRenameDropzone.TabIndex = 2;
+            this.lblCarRenameDropzone.Text = "Drop a car folder to add a prefix to the textures";
+            this.lblCarRenameDropzone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCarRenameDropzone.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblCarRenameDropzone_DragDrop);
+            this.lblCarRenameDropzone.DragOver += new System.Windows.Forms.DragEventHandler(this.lblCarRenameDropzone_DragOver);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 473);
-            this.Controls.Add(this.lblTWTDropzone);
+            this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(512, 512);
             this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(8);
             this.Text = "C2 Toolkit";
+            this.tabControl1.ResumeLayout(false);
+            this.tpMulti.ResumeLayout(false);
+            this.tpRename.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -66,5 +133,9 @@ namespace c2toolkit
         #endregion
 
         private System.Windows.Forms.Label lblTWTDropzone;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tpMulti;
+        private System.Windows.Forms.TabPage tpRename;
+        private System.Windows.Forms.Label lblCarRenameDropzone;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -35,50 +35,20 @@ namespace c2toolkit
                 {
                     foreach (string file in files)
                     {
-                        string fileExtension = Path.GetExtension(file);
+                        string fileExtension = Path.GetExtension(file).ToLower();
 
-                        if (fileExtension.ToLower() == ".twt")
+                        switch (fileExtension)
                         {
-                            ExtractTWT(file);
-                        }
-                        else if (fileExtension.ToLower() == ".p16" || fileExtension.ToLower() == ".p08")
-                        {
-                            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
-                            string srcDir = Path.GetDirectoryName(file);
-
-                            ProcessStartInfo startInfo = new ProcessStartInfo
-                            {
-                                FileName = Path.Combine(exeDirectory, "Resources\\TRixx.exe"),
-                                Arguments = "\"" + file + "\"",
-                                WorkingDirectory = srcDir,
-                                RedirectStandardInput = true,
-                                RedirectStandardOutput = true,
-                                UseShellExecute = false
-                            };
-                            Process p = Process.Start(startInfo);
-                            while (!p.StandardOutput.EndOfStream)
-                            {
-                                string line = p.StandardOutput.ReadLine();
-                                if (line != null && line.Contains("any key"))
-                                {
-                                    p.StandardInput.Write('y');
-                                    break;
-                                }
-                            }
-                            p.WaitForExit();
-
-                            try
-                            {
-                                Directory.Move(Path.Combine(srcDir, "pixies"), Path.Combine(srcDir, "tiffrgb"));
-                            }
-                            catch (Exception ex)
-                            {
-                                MessageBox.Show("tiffrgb folder probably already exists!", "C2 Toolkit", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            }
-                        }
-                        else
-                        {
-                            CreateTWT(file);
+                            case ".twt":
+                                ExtractTWT(file);
+                                break;
+                            case ".p16":
+                            case ".p08":
+                                UnpackPixies(file);
+                                break;
+                            default:
+                                CreateTWT(file);
+                                break;
                         }
                     }
                 }
@@ -117,6 +87,155 @@ namespace c2toolkit
             }
 
             newTWT.Save();
+        }
+
+        private void UnpackPixies(string file)
+        {
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string srcDir = Path.GetDirectoryName(file);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine(exeDirectory, "Resources\\TRixx.exe"),
+                Arguments = "\"" + file + "\"",
+                WorkingDirectory = srcDir,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            Process p = Process.Start(startInfo);
+            while (!p.StandardOutput.EndOfStream)
+            {
+                string line = p.StandardOutput.ReadLine();
+                if (line != null && line.Contains("any key"))
+                {
+                    p.StandardInput.Write('y');
+                    break;
+                }
+            }
+            p.WaitForExit();
+
+            try
+            {
+                Directory.Move(Path.Combine(srcDir, "pixies"), Path.Combine(srcDir, "tiffrgb"));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("tiffrgb folder probably already exists!", "C2 Toolkit", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lblCarRenameDropzone_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+                if (files != null && files.Length > 0)
+                {
+                    foreach (string file in files)
+                    {
+                        if (Directory.Exists(file))
+                        {
+                            string prefix = "";
+                            DialogResult dlr = ShowInputDialog("Enter prefix for " + Path.GetFileNameWithoutExtension(file), ref prefix);
+                            prefix = prefix.Trim();
+                            if (dlr != DialogResult.OK || prefix == string.Empty)
+                            {
+                                continue;
+                            }
+
+                            string tiffFolder = Path.Combine(file, "tiffrgb");
+                            string[] textureFiles = Directory.GetFiles(tiffFolder);
+                            foreach (string textureFile in textureFiles)
+                            {
+                                string textureFileName = Path.GetFileName(textureFile);
+                                string newName = Path.Combine(tiffFolder, prefix.ToUpper() + textureFileName);
+                                File.Move(textureFile, newName);
+                            }
+
+                            string[] carFiles = Directory.GetFiles(file);
+                            foreach (string carFile in carFiles)
+                            {
+                                string carFileExtension = Path.GetExtension(carFile).ToLower();
+                                switch (carFileExtension)
+                                {
+                                    case ".mat":
+                                        MAT carMaterial = MAT.Load(carFile);
+                                        foreach (MATMaterial m in carMaterial.Materials)
+                                        {
+                                            if (m.HasTexture)
+                                            {
+                                                m.Texture = prefix + m.Texture;
+                                            }
+                                        }
+
+                                        carMaterial.Save(carFile);
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "C2 Toolkit", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lblCarRenameDropzone_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private DialogResult ShowInputDialog(string title, ref string input)
+        {
+            System.Drawing.Size size = new System.Drawing.Size(512, 70);
+            Form inputBox = new Form();
+
+            inputBox.FormBorderStyle = FormBorderStyle.Fixed3D;
+            inputBox.ClientSize = size;
+            inputBox.Text = title;
+            inputBox.StartPosition = FormStartPosition.CenterParent;
+
+            TextBox textBox = new TextBox();
+            textBox.Size = new System.Drawing.Size(size.Width - 10, 23);
+            textBox.Location = new System.Drawing.Point(5, 5);
+            textBox.Text = input;
+            inputBox.Controls.Add(textBox);
+
+            Button okButton = new Button();
+            okButton.DialogResult = DialogResult.OK;
+            okButton.Name = "okButton";
+            okButton.Size = new System.Drawing.Size(75, 23);
+            okButton.Text = "&OK";
+            okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
+            inputBox.Controls.Add(okButton);
+
+            Button cancelButton = new Button();
+            cancelButton.DialogResult = DialogResult.Cancel;
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new System.Drawing.Size(75, 23);
+            cancelButton.Text = "&Cancel";
+            cancelButton.Location = new System.Drawing.Point(size.Width - 80, 39);
+            inputBox.Controls.Add(cancelButton);
+
+            inputBox.AcceptButton = okButton;
+            inputBox.CancelButton = cancelButton;
+
+            DialogResult result = inputBox.ShowDialog();
+            input = textBox.Text;
+            return result;
         }
     }
 }
